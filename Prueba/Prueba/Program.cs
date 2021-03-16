3fb9e2b81e3a474d91f744be16b0b66534b343c4
Pau Mezzadra <paumezzadra@hotmail.com>
2021-03-16 17:02:55 -0300

    Prueba

---
+++ Prueba/Prueba/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su nombre:");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Su nombre es: " + nombre);
+            Console.WriteLine("Presione cualquier tecla para continuar");
+            Console.ReadKey();
+        }
+    }
+}
